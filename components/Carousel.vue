e663d3cc4b6236915f538f362ac663d5ed3c74b8
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-06-05 23:32:09 +0300

    add images

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 const cards = ref([
-  ['This is something', 'https://images.pexels.com/photos/1141853/pexels-photo-1141853.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
-  ['Very special', 'https://images.pexels.com/photos/1258865/pexels-photo-1258865.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
-  ['Special is the key', 'https://images.pexels.com/photos/808466/pexels-photo-808466.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
-  ['For you', 'https://images.pexels.com/photos/1394841/pexels-photo-1394841.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
-  ['Just give it', 'https://images.pexels.com/photos/969679/pexels-photo-969679.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
-  ['A try', 'https://images.pexels.com/photos/1834400/pexels-photo-1834400.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
-  ['And see', 'https://images.pexels.com/photos/1415268/pexels-photo-1415268.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
-  ['How IT Works', 'https://images.pexels.com/photos/135018/pexels-photo-135018.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
-  ['Woow', 'https://images.pexels.com/photos/1175135/pexels-photo-1175135.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'],
+  'Здравствуйте',
+  'Меня зовут Максим',
+  'И я открыточный аферист',
+  'Я обманываю людей',
+  'Ввожу их в заблуждение',
+  'Внушаю надежду',
+  'Веру в открытку',
+  'А потом',
+  'Не дарюю её 7 лет',
 ])
 
 const width = ref(window.innerWidth)
@@ -27,11 +27,11 @@ const spacer = computed(() => scale.value * 420 + 'px')
   <div class="container">
     <div class="carousel">
       <div
-        v-for="([title, url], index) in cards"
+        v-for="(title, index) in cards"
         :key="title"
         class="card"
         :style='`
-          background-image: url(${url});
+          background-image: url(/carousel/${index + 1}.jpeg);
           transform: rotateY(${index * Math.floor(360 / cards.length)}deg) translateZ(430px);
         `'
       >
@@ -79,6 +79,8 @@ const spacer = computed(() => scale.value * 420 + 'px')
   background-size: cover;
   box-shadow:inset 0 0 0 2000px rgba(0,0,0,0.3);
   display: flex;
+  background-repeat:no-repeat;
+  background-position: center center;
 
   span {
     margin: auto;
