9f7eab6d5de4cf2eec7826ea1c5ea7b0c4d7a66c
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-06-05 21:54:14 +0300

    add head data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
   ssr: false,
   target: 'static',
+  app: {
+    head: {
+      title: 'Максим-максим',
+      link: [{ rel: "icon", type: "image/x-icon", href: "/icon.png" }],
+      meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0' }]
+    }
+  },
 })
